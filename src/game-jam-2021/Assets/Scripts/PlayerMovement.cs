bb581a54a45c4ff282dc87f29beedda6ee27f82b
lull-by <jeffery.li316@gmail.com>
2021-07-10 21:10:45 -0400

    added player walk and idle animations

--- src/game-jam-2021/Assets/Scripts/PlayerMovement.cs
+++ src/game-jam-2021/Assets/Scripts/PlayerMovement.cs
@@ -4,23 +4,44 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-
+    public float walkSpeed = 5f;
+    public float acceleration = 1f;
     public Rigidbody2D rb;
+    public Animator animator;
 
-    private Vector2 movement;
+
+    private Vector2 direction;
+    private Vector2 input;
+    private float currentWalkSpeed = 0f;
 
     void Update()
     {
         // Input
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        movement = movement.normalized;
-        Debug.Log($"{movement.x}, {movement.y}");
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        bool isWalking = input.x != 0 || input.y != 0;
+
+        Debug.Log($"walkSpeed: {currentWalkSpeed} hor: {input.x} vert: {input.y}");
+
+        if (isWalking) {
+            // accelerate
+            currentWalkSpeed = walkSpeed;
+
+            direction = input.normalized;
+            animator.SetFloat("dirHorizontal", direction.x);
+            animator.SetFloat("dirVertical", direction.y);
+        }
+        else {
+            // decelerate
+            currentWalkSpeed = 0;
+        }
+
+        animator.SetBool("isWalking", isWalking);
+        animator.SetFloat("walkSpeed", currentWalkSpeed);
     }
 
     void FixedUpdate() {
         // Movement
-        rb.velocity = movement * moveSpeed;
+        rb.velocity = direction * currentWalkSpeed;
     }
 }
